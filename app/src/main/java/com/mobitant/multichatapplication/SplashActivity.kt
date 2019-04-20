5cca6cb7bdb9ce8d72bd62a8f0466d71ec67a7ca
ganghee <idiosauri@naver.com>
2019-04-20 16:01:55 +0900

    처음 나오는 화면 구현
    서버 점검 remote config를 이용하여 서버 점검중이라는 dialog가 나오게 할 수 있다.

--- app/src/main/java/com/mobitant/multichatapplication/SplashActivity.kt
+++ app/src/main/java/com/mobitant/multichatapplication/SplashActivity.kt
@@ -1,22 +1,31 @@
 package com.mobitant.multichatapplication
 import android.graphics.Color
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.WindowManager
 import android.widget.Toast
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import kotlinx.android.synthetic.main.activity_splash.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
 
-    lateinit var remoteConfig : FirebaseRemoteConfig
+    private lateinit var remoteConfig : FirebaseRemoteConfig
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        //상태바를 없애고 화면 전체를 차지하게 설정
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+
+        // Remote Config의 설정 값을 이용하여 splash화면에서 대기시간과 splash_message_caps이
+        // true인 경우에 '서버 점검 중' 이라는 Dialog가 나온뒤 화면이 꺼진다.
+        //
         remoteConfig = FirebaseRemoteConfig.getInstance()
         val configSettings = FirebaseRemoteConfigSettings.Builder()
             .setDeveloperModeEnabled(BuildConfig.DEBUG)
@@ -27,7 +36,7 @@ class SplashActivity : AppCompatActivity() {
         remoteConfig.fetchAndActivate()
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    val updated = task.getResult()
+                    val updated = task.result
                     Log.d(TAG,"Config params updated: $updated")
                     Toast.makeText(this, "Fetch and activate succeeded",
                         Toast.LENGTH_SHORT).show()
@@ -39,21 +48,21 @@ class SplashActivity : AppCompatActivity() {
             }
     }
     private fun displayWelcomeMessage() {
+
         val splash_background = remoteConfig.getString("splash_background")
         val caps = remoteConfig.getBoolean("splash_message_caps")
-        val splash_message = remoteConfig.getString("splash_message")
+        val message = remoteConfig.getString("splash_message")
 
         splashactivity_linearlayout.setBackgroundColor(Color.parseColor(splash_background))
 
         if(caps){
-            val builder = AlertDialog.Builder(this@SplashActivity)
-            builder.setMessage(splash_message)
-            builder.setPositiveButton("YES"){dialog, which ->
+            val builder = alert(message)
+            builder.positiveButton("YES") {
                 finish()
             }
-            val dialog: AlertDialog = builder.create()
-
-            dialog.show()
+            builder.show()
+        }else{
+            startActivity<LoginActivity>()
         }
     }
 
@@ -61,9 +70,5 @@ class SplashActivity : AppCompatActivity() {
 
         private const val TAG = "SplashActivity"
 
-        // Remote Config keys
-        private const val LOADING_PHRASE_CONFIG_KEY = "loading_phrase"
-        private const val WELCOME_MESSAGE_KEY = "welcome_message"
-        private const val WELCOME_MESSAGE_CAPS_KEY = "welcome_message_caps"
     }
 }
