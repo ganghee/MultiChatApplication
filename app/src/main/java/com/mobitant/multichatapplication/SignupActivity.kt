3fa1b54b5db001754dea35e149ec4ce5e1ee009b
ganghee <idiosauri@naver.com>
2019-04-21 00:54:19 +0900

    회원가입 화면
    이미지와 아이디 비밀번호 이름을 입력하고 firebase서버에 저장한다.

--- app/src/main/java/com/mobitant/multichatapplication/SignupActivity.kt
+++ app/src/main/java/com/mobitant/multichatapplication/SignupActivity.kt
@@ -1,11 +1,15 @@
 package com.mobitant.multichatapplication
 
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.storage.FirebaseStorage
 import com.mobitant.multichatapplication.model.UserModel
 import kotlinx.android.synthetic.main.activity_signup.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
@@ -13,7 +17,8 @@ import org.jetbrains.anko.sdk27.coroutines.onClick
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var remoteConfig : FirebaseRemoteConfig
-
+    val PICK_FROM_ALBUM = 10
+    private lateinit var imageUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,8 @@ class SignupActivity : AppCompatActivity() {
         val splash_background:String = remoteConfig.getString(getString(R.string.rc_color))
         window.statusBarColor = Color.parseColor(splash_background)
         signupActivity_button_signup.setBackgroundColor(Color.parseColor(splash_background))
+
+
 
         //회원가입 버튼 클릭시 이벤트
         //email, name, password가 null이 아닐 때 FirebaseAuth의 인증을 한다.
@@ -45,18 +52,38 @@ class SignupActivity : AppCompatActivity() {
                 signupActivity_edittext_email.text.toString(),
                 signupActivity_edittext_password.text.toString())
                 .addOnCompleteListener(this@SignupActivity){task->
-                    val userModel = UserModel()
-                    userModel.userName = signupActivity_edittext_name!!.text.toString()
+
 
                     val uid = task.result!!.user.uid
-                    FirebaseDatabase.getInstance().reference.child("users").child(uid).setValue(userModel)
+
+                    //firebaseStorage에 이미지가 저장된다.
+                    FirebaseStorage.getInstance().reference.child("userImage").child(uid).putFile(imageUri!!)
+                        .addOnCompleteListener{task ->
+                            val imageUri = task.result.toString()
+                            val userModel = UserModel()
+                            userModel.userName = signupActivity_edittext_name!!.text.toString()
+                            userModel.profieImageUrl = imageUri
+
+                            FirebaseDatabase.getInstance().reference.child("users").child(uid).setValue(userModel)
                 }
-
-
         }
-
-
-
     }
 
+        //이미지를 클릭하는 경우 앨범에서 이미지 가져오기
+        //화면 전환과 함께 PICK_FROM_ALBUM 정보도 넘겨준다.
+        signupActivity_imageview_profile.onClick {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = MediaStore.Images.Media.CONTENT_TYPE
+            startActivityForResult(intent,PICK_FROM_ALBUM)
+        }
+    }
+
+    //사용자가 이미지를 선택하면 뷰를 바꾸고 이미지 경로에 이미지 원본 저장
+    //데이터를 가진 이미지 경로는 회원가입 버튼 클릭시 firebaseStorage에 저장된다.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == PICK_FROM_ALBUM && resultCode == RESULT_OK){
+            signupActivity_imageview_profile.setImageURI(data!!.data)//가운데 뷰를 바꿈
+            imageUri = data.data  //이미지 경로 원본
+        }
+    }
 }
